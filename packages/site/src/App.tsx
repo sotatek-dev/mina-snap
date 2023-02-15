934eeb6d7c0645f8fb5bdb8c83293b92541dced2
Sotatek-DuyNguyen2 <duy.nguyen2@sotatek.com>
2023-02-15 15:34:21 +0700

    feat: init dapp

--- packages/site/src/App.tsx
+++ packages/site/src/App.tsx
@@ -1,33 +1,79 @@
-import { FunctionComponent, ReactNode, useContext } from 'react';
-import styled from 'styled-components';
-import { Footer, Header } from './components';
+import { useEffect } from 'react';
+import './App.css';
+import GlobalStyle from 'theme/GlobalStyles';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Home } from 'components/pages/Home';
+import { FrameworkView } from 'components/ui/Framework/Framework.view';
+import { useAppSelector } from 'hooks/redux';
+import { useStarkNetSnap } from 'services';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/default';
+import { ConnectModal } from 'components/ui/organism/ConnectModal';
+import { PopIn } from 'components/ui/molecule/PopIn';
+import { LoadingBackdrop } from 'components/ui/molecule/LoadingBackdrop';
+import { ConnectInfoModal } from 'components/ui/organism/ConnectInfoModal';
+import 'toastr2/dist/toastr.min.css';
+import { NoFlaskModal } from 'components/ui/organism/NoFlaskModal';
+import { MinVersionModal } from './components/ui/organism/MinVersionModal';
+import { useHasMetamaskFlask } from 'hooks/useHasMetamaskFlask';
 
-import { GlobalStyle } from './config/theme';
-import { ToggleThemeContext } from './Root';
+library.add(fas, far);
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100vh;
-  max-width: 100vw;
-`;
+function App() {
+  const { initSnap, getWalletData, checkConnection } = useStarkNetSnap();
+  const { connected, forceReconnect } = useAppSelector((state) => state.wallet);
+  const { infoModalVisible, minVersionModalVisible } = useAppSelector((state) => state.modals);
+  const { loader } = useAppSelector((state) => state.UI);
+  const networks = useAppSelector((state) => state.networks);
+  const { accounts } = useAppSelector((state) => state.wallet);
+  const { hasMetamaskFlask } = useHasMetamaskFlask();
 
-export type AppProps = {
-  children: ReactNode;
-};
+  const address = accounts?.length > 0 ? (accounts[0] as unknown as string) : '0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-export const App: FunctionComponent<AppProps> = ({ children }) => {
-  const toggleTheme = useContext(ToggleThemeContext);
+  useEffect(() => {
+    if (connected) {
+      initSnap();
+    }
+    if (hasMetamaskFlask && !connected && !forceReconnect) {
+      checkConnection();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connected, forceReconnect, hasMetamaskFlask]);
+
+  useEffect(() => {
+    if (networks.items.length > 0) {
+      const chainId = networks.items[networks.activeNetwork].chainId;
+      const useOldAccounts = !!networks.items[networks.activeNetwork].useOldAccounts;
+      getWalletData(chainId, useOldAccounts);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [networks.activeNetwork]);
+
+  const loading = loader.isLoading;
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Wrapper>
-        <Header handleToggleClick={toggleTheme} />
-        {children}
-        <Footer />
-      </Wrapper>
-    </>
+      <FrameworkView connected={connected}>
+        <PopIn isOpen={!connected && !loading} showClose={false}>
+          <NoFlaskModal />
+        </PopIn>
+        <PopIn isOpen={minVersionModalVisible} showClose={false}>
+          <MinVersionModal />
+        </PopIn>
+        <PopIn isOpen={!loading && !!hasMetamaskFlask && !connected} showClose={false}>
+          <ConnectModal />
+        </PopIn>
+        <PopIn isOpen={infoModalVisible} showClose={false}>
+          <ConnectInfoModal address={address} />
+        </PopIn>
+        <Home address={address} />
+        <PopIn isOpen={loading}>{loading && <LoadingBackdrop>{loader.loadingMessage}</LoadingBackdrop>}</PopIn>
+      </FrameworkView>
+    </ThemeProvider>
   );
-};
+}
+
+export default App;
--- packages/site/src/config/snap.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The snap origin to use.
- * Will default to the local hosted snap if no value is provided in environment.
- */
-export const defaultSnapOrigin =
-  process.env.REACT_APP_SNAP_ORIGIN ?? `local:http://localhost:8080`;
-// 'npm:test-mina-snap'
