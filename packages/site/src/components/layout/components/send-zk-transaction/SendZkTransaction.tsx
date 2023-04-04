c1b4809c02079d7f12e7046e67dbab96fe995108
Sotatek-HieuMai <hieu.mai@sotatek.com>
2023-04-04 11:35:40 +0700

    add function call zkApp

--- packages/site/src/components/layout/components/send-zk-transaction/SendZkTransaction.tsx
+++ packages/site/src/components/layout/components/send-zk-transaction/SendZkTransaction.tsx
@@ -3,6 +3,14 @@ import Button from 'components/common/button';
 import ModalCommon from 'components/common/modal';
 import React from 'react';
 import styled from 'styled-components';
+import {
+  Mina,
+  isReady,
+  PublicKey,
+  PrivateKey,
+  fetchAccount
+} from 'snarkyjs';
+// import type { Add } from '../../../../../public/smart-contract/Add'
 
 interface ModalProps {
   open: boolean;
@@ -13,6 +21,56 @@ interface ModalProps {
 type ContainerProps = React.PropsWithChildren<Omit<ModalProps, 'closeSucces'>>;
 
 const SendZkTransaction = ({ open, clickOutSide, setOpenModal }: ModalProps) => {
+
+  const submitZkTransaction = async () => { 
+    console.log('-start');
+    await isReady;
+      const { Add } = await import('smart-contract');
+
+      // Update this to use the address (public key) for your zkApp account
+      // To try it out, you can try this address for an example "Add" smart contract that we've deployed to 
+      // Berkeley Testnet B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV
+      const senderPrivateKey = 'EKEEPTX3EKjQBscJiNZoZEMgiTk87VNF7W9Ey5XFwtC9GEhj9yfQ';
+      const senderAddress = 'B62qiwy4VoVLzBDdSC7dwN6agu6fe4QKqdzbFvWZ2dEmsiddX32A1tC';
+
+      const zkAppPrivateKey = 'EKEfFXajzqebC4yEQzxaqaov9JgMMyeeAdNuuF84UVWVbzJrMjMh';
+      // This should be removed once the zkAppAddress is updated.
+      const zkAppAddress = 'B62qokxHiP7MGhmqztYdEbfXU7kqLztrKaDwftZ8ZiLM86mjYr2LNuT';
+
+      if (!zkAppAddress) {
+        console.error(
+          'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4'
+        );
+      }
+      const zkApp = new Add(PublicKey.fromBase58(zkAppAddress));
+      console.log('zkApp', zkApp);
+      
+      Mina.setActiveInstance(Mina.Network('https://proxy.berkeley.minaexplorer.com/graphql'));
+      await Add.compile();
+      const account = await fetchAccount({publicKey: zkAppAddress, ...zkApp}, 'https://proxy.berkeley.minaexplorer.com/graphql');
+      console.log(`-account:`, account);
+      let tx = await Mina.transaction({ sender: PublicKey.fromBase58(senderAddress), fee: 0.1e9 }, () => {
+        zkApp.update();
+      });
+      console.log(`tx:`, tx);
+      const provedTx = await tx.prove();
+      console.log('send transaction...', provedTx);
+      let sentTx = await tx.sign([PrivateKey.fromBase58(senderPrivateKey)]).send();
+      console.log('sentTx:', sentTx);
+
+      if (sentTx.hash() !== undefined) {
+        console.log(`
+      Success! Update transaction sent.
+      
+      Your smart contract state will be updated
+      as soon as the transaction is included in a block:
+      https://berkeley.minaexplorer.com/transaction/${sentTx.hash()}
+      `);
+      }
+
+    console.log('-end');
+
+  }
   return (
     <Modal
       open={open}
@@ -39,7 +97,7 @@ const SendZkTransaction = ({ open, clickOutSide, setOpenModal }: ModalProps) => 
                 }}
               />
             </Content>
-            <Button>Send</Button>
+            <Button onClick={() => submitZkTransaction()}>Send</Button>
         </Wrapper>
     </Modal>
   );
