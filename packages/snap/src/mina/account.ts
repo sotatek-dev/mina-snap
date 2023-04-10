cca11d7805a70062ae10b4f211ec54d53240af6e
Sotatek-ThanhVu <thanh.vu@sotatek>
2023-04-10 11:29:20 +0700

    fix: update graphql response

--- packages/snap/src/mina/account.ts
+++ packages/snap/src/mina/account.ts
@@ -91,7 +91,7 @@ export const signMessage = async (message: string, keypair: Keypair, networkConf
  * @returns `null` if get account info fail.
  */
 export async function getAccountInfo(publicKey: string, networkConfig: NetworkConfig) {
-  const query = getAccountInfoQuery;
+  const query = getAccountInfoQuery();
   const variables = { publicKey };
 
   const data = await gql(networkConfig.gqlUrl, query, variables);
@@ -159,7 +159,7 @@ export const createAccount = async (name: string, index?: number): Promise<any> 
   const { publicKey } = await generateKeyPair(networks[currentNetwork], newAccountIndex);
   const duplicateAddress = checkDuplicateAddress(networks[currentNetwork], publicKey);
   if (duplicateAddress) {
-    return createAccount(name, newAccountIndex + 1)
+    return createAccount(name, newAccountIndex + 1);
   }
   snapConfig.networks[currentNetwork].currentAccIndex = newAccountIndex;
   snapConfig.networks[currentNetwork].selectedImportedAccount = null;
@@ -214,24 +214,24 @@ export const getAccounts = async () => {
   const generatedAccountsArr =
     Object.keys(generatedAccounts).length > 0
       ? Object.entries(generatedAccounts).map(([index, account]) => {
-          return {
-            ...account,
-            index,
-            isImported: false,
-          } as any;
-        })
+        return {
+          ...account,
+          index,
+          isImported: false,
+        } as any;
+      })
       : [];
   const importedAccountsArr =
     Object.keys(importedAccounts).length > 0
       ? Object.entries(importedAccounts).map(([index, account]) => {
-          const { name, address } = account;
-          return {
-            name,
-            address,
-            index,
-            isImported: true,
-          } as any;
-        })
+        const { name, address } = account;
+        return {
+          name,
+          address,
+          index,
+          isImported: true,
+        } as any;
+      })
       : [];
   const allAccounts = [...generatedAccountsArr, ...importedAccountsArr];
   await Promise.all(
@@ -265,7 +265,7 @@ export const editAccountName = async (index: number, name: string, isImported?: 
 export const verifyMessage = (networkConfig: NetworkConfig, signedData: Signed<Message>) => {
   const client = getMinaClient(networkConfig);
   return client.verifyMessage(signedData);
-}
+};
 
 const checkDuplicateAddress = (networkConfig: NetworkConfig, publicKey: string) => {
   const { generatedAccounts, importedAccounts } = networkConfig;
@@ -275,4 +275,4 @@ const checkDuplicateAddress = (networkConfig: NetworkConfig, publicKey: string) 
   ];
   const duplicateAddress = existingAddresses.find((address) => address === publicKey);
   return duplicateAddress;
-}
+};
