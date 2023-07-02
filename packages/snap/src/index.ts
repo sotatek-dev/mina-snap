751cd4bbf041eca50fb9c0754c07652cdff4e5da
SotaTek-TungNguyen2 <tung.nguyen2@sotatek.com>
2023-07-02 09:32:42 +0700

    refactor: wrap onRpcRequest content into mutex.runExclusive

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -40,172 +40,157 @@ import { Mutex } from 'async-mutex';
  */
 const mutex = new Mutex();
 export const onRpcRequest: OnRpcRequestHandler = async ({ request, origin }) => {
-  if (mutex.isLocked()) {
-    await mutex.waitForUnlock();
-  }
-  const snapConfig = await getSnapConfiguration();
-  const networkConfig = await getNetworkConfig(snapConfig);
-  if (Object.keys(networkConfig.generatedAccounts).length === 0 && !mutex.isLocked()) {
-    await mutex.runExclusive(async () => {
+  return mutex.runExclusive(async () => {
+    const snapConfig = await getSnapConfiguration();
+    const networkConfig = await getNetworkConfig(snapConfig);
+    if (Object.keys(networkConfig.generatedAccounts).length === 0) {
       await createAccount('Account 1');
-    });
-  }
-  switch (request.method) {
-    case EMinaMethod.ACCOUNT_INFO: {
-      const { publicKey, name } = await getKeyPair();
-      const { account } = await getAccountInfo(publicKey, networkConfig);
-      account.name = name;
-      return account;
     }
+    switch (request.method) {
+      case EMinaMethod.ACCOUNT_INFO: {
+        const { publicKey, name } = await getKeyPair();
+        const { account } = await getAccountInfo(publicKey, networkConfig);
+        account.name = name;
+        return account;
+      }
 
-    case EMinaMethod.ACCOUNT_LIST: {
-      const accounts = await getAccounts();
-      return accounts;
-    }
+      case EMinaMethod.ACCOUNT_LIST: {
+        const accounts = await getAccounts();
+        return accounts;
+      }
 
-    case EMinaMethod.CREATE_ACCOUNT: {
-      return mutex.runExclusive(async () => {
+      case EMinaMethod.CREATE_ACCOUNT: {
         const { name } = request.params as { name: string };
         const account = await createAccount(name);
         return account;
-      });
-    }
+      }
 
-    case EMinaMethod.EDIT_ACCOUNT_NAME: {
-      return mutex.runExclusive(async () => {
+      case EMinaMethod.EDIT_ACCOUNT_NAME: {
         const { index, name, isImported } = request.params as { index: number; name: string; isImported?: boolean };
         const account = await editAccountName(index, name, isImported);
         return account;
-      });
-    }
+      }
 
-    case EMinaMethod.CHANGE_NETWORK: {
-      return mutex.runExclusive(async () => {
+      case EMinaMethod.CHANGE_NETWORK: {
         const { networkName } = request.params as { networkName: ENetworkName };
         const newNetwork = await changeNetwork(networkName);
         return newNetwork;
-      });
-    }
+      }
 
-    case EMinaMethod.CHANGE_ACCOUNT: {
-      return mutex.runExclusive(async () => {
+      case EMinaMethod.CHANGE_ACCOUNT: {
         const { accountIndex, isImported } = request.params as { accountIndex: number; isImported?: boolean };
         const accountInfo = await changeAccount(accountIndex, isImported);
         return accountInfo;
-      });
-    }
+      }
 
-    case EMinaMethod.IMPORT_ACCOUNT_PK: {
-      return mutex.runExclusive(async () => {
+      case EMinaMethod.IMPORT_ACCOUNT_PK: {
         const { name, privateKey } = request.params as { name: string; privateKey: string };
         const accountInfo = await importAccount(name, privateKey);
         return accountInfo;
-      });
-    }
-
-    case EMinaMethod.EXPORT_PRIVATE_KEY: {
-      const { index, isImported } = request.params as { index?: number; isImported?: boolean };
-      const confirmation = await popupDialog(
-        ESnapDialogType.CONFIRMATION,
-        `Do you want to export your private key?`,
-        `Warning: Never disclose this key. Anyone with your private keys can steal any assets held in your account.\n(Request origin: ${origin})`,
-      );
-      if (!confirmation) {
-        await popupNotify('Exporting private key is rejected');
-        return null;
-      } else {
-        const { privateKey } = await getKeyPair(index, isImported);
-        return { privateKey };
       }
-    }
 
-    case EMinaMethod.NETWORK_CONFIG: {
-      const { name, gqlUrl, gqlTxUrl, explorerUrl, token } = networkConfig;
-      return {
-        name,
-        gqlUrl,
-        gqlTxUrl,
-        explorerUrl,
-        token,
-      };
-    }
-
-    case EMinaMethod.SEND_PAYMENT: {
-      const txInput = request.params as TxInput;
-      const response = await sendPayment(txInput, networkConfig);
-
-      return response;
-    }
-
-    case EMinaMethod.SIGN_MESSAGE: {
-      const keyPair = await getKeyPair();
-      const { message } = request.params as { message: string };
-      const signature = await signMessage(message, keyPair, networkConfig);
-
-      return signature;
-    }
-
-    case EMinaMethod.RESET_CONFIG: {
-      const confirmation = await popupDialog(
-        ESnapDialogType.CONFIRMATION,
-        `Do you want to reset the Mina snap config?`,
-        `This will clear all your Mina snap configs on this device and may result in loss of access to accounts. \n(Request origin: ${origin})`,
-      );
-      if (!confirmation) {
-        await popupNotify('Reset snap config is rejected');
-        return null;
+      case EMinaMethod.EXPORT_PRIVATE_KEY: {
+        const { index, isImported } = request.params as { index?: number; isImported?: boolean };
+        const confirmation = await popupDialog(
+          ESnapDialogType.CONFIRMATION,
+          `Do you want to export your private key?`,
+          `Warning: Never disclose this key. Anyone with your private keys can steal any assets held in your account.\n(Request origin: ${origin})`,
+        );
+        if (!confirmation) {
+          await popupNotify('Exporting private key is rejected');
+          return null;
+        } else {
+          const { privateKey } = await getKeyPair(index, isImported);
+          return { privateKey };
+        }
       }
-      return mutex.runExclusive(async () => {
+
+      case EMinaMethod.NETWORK_CONFIG: {
+        const { name, gqlUrl, gqlTxUrl, explorerUrl, token } = networkConfig;
+        return {
+          name,
+          gqlUrl,
+          gqlTxUrl,
+          explorerUrl,
+          token,
+        };
+      }
+
+      case EMinaMethod.SEND_PAYMENT: {
+        const txInput = request.params as TxInput;
+        const response = await sendPayment(txInput, networkConfig);
+
+        return response;
+      }
+
+      case EMinaMethod.SIGN_MESSAGE: {
+        const keyPair = await getKeyPair();
+        const { message } = request.params as { message: string };
+        const signature = await signMessage(message, keyPair, networkConfig);
+
+        return signature;
+      }
+
+      case EMinaMethod.RESET_CONFIG: {
+        const confirmation = await popupDialog(
+          ESnapDialogType.CONFIRMATION,
+          `Do you want to reset the Mina snap config?`,
+          `This will clear all your Mina snap configs on this device and may result in loss of access to accounts. \n(Request origin: ${origin})`,
+        );
+        if (!confirmation) {
+          await popupNotify('Reset snap config is rejected');
+          return null;
+        }
         const defaultConfig = await resetSnapConfiguration();
         return defaultConfig;
-      })
+      }
+
+      case EMinaMethod.GET_TX_HISTORY: {
+        const keyPair = await getKeyPair();
+        const history = await getTxHistory(networkConfig, request.params as HistoryOptions, keyPair.publicKey);
+
+        return history;
+      }
+
+      case EMinaMethod.GET_TX_DETAIL: {
+        const { hash } = request.params as { hash: string };
+        const payment = await getTxDetail(networkConfig, hash);
+
+        return payment;
+      }
+
+      case EMinaMethod.GET_TX_STATUS: {
+        const { paymentId } = request.params as { paymentId: string };
+        const status = await getTxStatus(networkConfig, paymentId);
+
+        return status;
+      }
+
+      case EMinaMethod.VERIFY_MESSAGE: {
+        const signedData = request.params as SignedLegacy<any>;
+        const verifyResult = verifyMessage(networkConfig, signedData);
+        return verifyResult;
+      }
+
+      case EMinaMethod.SEND_STAKE_DELEGATION: {
+        const stakeTxInput = request.params as StakeTxInput;
+        const response = await sendStakeDelegation(stakeTxInput, networkConfig);
+
+        return response;
+      }
+
+      case EMinaMethod.REQUEST_NETWORK_NAME: {
+        return networkConfig.name;
+      }
+
+      case EMinaMethod.SEND_TX: {
+        const args = request.params as ZkAppTxInput;
+        const submitZkAppResult = await sendZkAppTx(args, networkConfig);
+        return submitZkAppResult;
+      }
+
+      default:
+        throw new Error('Method not found.');
     }
-
-    case EMinaMethod.GET_TX_HISTORY: {
-      const keyPair = await getKeyPair();
-      const history = await getTxHistory(networkConfig, request.params as HistoryOptions, keyPair.publicKey);
-
-      return history;
-    }
-
-    case EMinaMethod.GET_TX_DETAIL: {
-      const { hash } = request.params as { hash: string };
-      const payment = await getTxDetail(networkConfig, hash);
-
-      return payment;
-    }
-
-    case EMinaMethod.GET_TX_STATUS: {
-      const { paymentId } = request.params as { paymentId: string };
-      const status = await getTxStatus(networkConfig, paymentId);
-
-      return status;
-    }
-
-    case EMinaMethod.VERIFY_MESSAGE: {
-      const signedData = request.params as SignedLegacy<any>;
-      const verifyResult = verifyMessage(networkConfig, signedData);
-      return verifyResult;
-    }
-
-    case EMinaMethod.SEND_STAKE_DELEGATION: {
-      const stakeTxInput = request.params as StakeTxInput;
-      const response = await sendStakeDelegation(stakeTxInput, networkConfig);
-
-      return response;
-    }
-
-    case EMinaMethod.REQUEST_NETWORK_NAME: {
-      return networkConfig.name;
-    }
-
-    case EMinaMethod.SEND_TX: {
-      const args = request.params as ZkAppTxInput;
-      const submitZkAppResult = await sendZkAppTx(args, networkConfig);
-      return submitZkAppResult;
-    }
-
-    default:
-      throw new Error('Method not found.');
-  }
+  });
 };
